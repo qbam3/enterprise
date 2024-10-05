7d10ba600a6097837ff61d00e7f968479ad4f7c4
GodSpeeds27 <tuwowoxnl@gmail.com>
2024-10-05 11:28:58 +0700

    add admin by danang

--- routes/admin.js
+++ routes/admin.js
@@ -6,84 +6,90 @@ var model_kelas = require('../model/model_kelas')
 var model_matakuliah = require('../model/model_matakuliah')
 var model_jadwal = require('../model/model_jadwal')
 var model_users = require('../model/model_users')
+var model_dosen = require('../model/model_dosen')
+var model_ruangan = require('../model/model_ruangan')
 
 
+router.get('/submit', async (req, res, next)=>{
+    let DataUsers = await model_users.getAll()
+    let DataProdi = await model_prodi.getAll()
+    res.render('admin/submit',{
+       DataUsers,
+       DataProdi,
+       role: DataUsers[0].role
+    })
+})
 router.get('/', async (req, res, next)=>{
-    let rows = await model_users.getAll()
+    let Data = await model_users.getAll()
     res.render('admin/index',{
-        data : rows,
-        username: Data[0].username
+        username: Data[0].username,
+        email: Data[0].email
     })
 })
 
-router.get('/create/semester', async (req, res, next)=>{
-    let rows = await model_semester.getAll()
-    res.render('',{
-        data : rows
-    })
-})
+router.post('/submitAll', async (req, res, next) => {
+    try {
+        console.log(req.body); 
 
-router.post('/post/semester', async ()=>{
-    let semester = req.body
-    let Data = {semester}
-    await model_semester.store(Data)
-    req.flash('success', 'berhasil memasukan data')
-})
+        let { nama_dosen, jenis_kelamin, agama, semester, prodi, nama_kelas, nama_mata_kuliah, jadwal_kuliah, kelas } = req.body;
 
-router.get('/create/prodi', async (req, res, next)=>{
-    let rows = await model_prodi.getAll()
-    res.render('',{
-        data : rows
-    })
-})
+        let id_users = req.session.userId; 
+        await model_dosen.store({
+            nama_dosen,
+            jenis_kelamin,
+            agama,
+            id_users,
+        });
 
-router.post('/post/prodi', async ()=>{
-    let {prodi, id_semester} = req.body
-    let Data = {prodi, id_semester}
-    await model_semester.store(Data)
-    req.flash('success', 'berhasil memasukan data')
-})
+        await model_semester.store({
+            semester,
+        });
+        let getSemester = await model_semester.getAll()
+        let id_semester = getSemester[0].id_semester;
 
-router.get('/create/kelas', async (req, res, next)=>{
-    let rows = await model_kelas.getAll()
-    res.render('',{
-        data : rows
-    })
-})
+        await model_prodi.store({
+            prodi,
+            id_semester,
+        });
+        let getprodi = await model_prodi.getAll()
+        let id_prodi = getprodi[0].id_prodi;
 
-router.post('/post/kelas', async ()=>{
-    let {nama_kelas, id_prodi} = req.body
-    let Data = {nama_kelas, id_prodi}
-    await model_kelas.store(Data)
-    req.flash('success', 'berhasil memasukan data')
-})
+        await model_kelas.store({
+            nama_kelas,
+            id_prodi,
+        });
+        let getKelas = await model_kelas.getAll()
+        let id_kelas = getKelas[0].id_kelas;
 
-router.get('/create/matakuliah', async (req, res, next)=>{
-    let rows = await model_matakuliah.getAll()
-    res.render('',{
-        data : rows
-    })
-})
+        await model_matakuliah.store({
+            nama_mata_kuliah,
+            id_kelas,
+        });
+        let getMatkul = await model_matakuliah.getAll()
+        let id_matakuliah = getMatkul[0].id_matakuliah;
 
-router.post('/post/matakuliah', async ()=>{
-    let {nama_mata_kuliah, id_kelas} = req.body
-    let Data = {nama_mata_kuliah, id_kelas}
-    await model_matakuliah.store(Data)
-    req.flash('success', 'berhasil memasukan data')
-})
+        let getDosen = await model_dosen.getAll()
+        let id_dosen = getDosen[0].id_dosen
+        await model_jadwal.store({
+            jadwal_kuliah,
+            id_kelas,
+            id_matakuliah,
+            id_dosen
+        });
+        let getJadwal = await model_jadwal.getAll()
+        let id_jadwal = getJadwal[0].id_jadwal
+        await model_ruangan.store({
+            kelas,
+            id_jadwal
+        });
 
-router.get('/create/jadwal', async (req, res, next)=>{
-    let rows = await model_jadwal.getAll()
-    res.render('',{
-        data : rows
-    })
-})
+        req.flash('success', 'Data Berhasil Ditambahkan');
+        res.redirect('/admin'); 
 
-router.post('/post/jadwal', async ()=>{
-    let {jadwal_kuliah, id_matakuliah, id_dosen, id_kelas} = req.body
-    let Data = {jadwal_kuliah, id_matakuliah, id_dosen, id_kelas}
-    await model_jadwal.store(Data)
-    req.flash('success', 'berhasil memasukan data')
-})
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Terjadi kesalahan saat menyimpan data');
+    }
+});
 
 module.exports = router
